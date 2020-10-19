0f73630ad4e5102d94e180626382ad521e67b24f
Victor Wong <victorw1992@gmail.com>
2020-10-19 10:52:44 -0700

    Created Grocery List with Cucumbers and Kale component

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // TODO
+var Cucumber = () => (
+  <li>Cucumber</li>
+);
+var Kale = () => (
+  <li>Kale</li>
+);
+
+var GroceryList = () => (
+  <ul>
+    <Cucumber />
+    <Kale />
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Grocery List List</h2>
+    <GroceryList />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
